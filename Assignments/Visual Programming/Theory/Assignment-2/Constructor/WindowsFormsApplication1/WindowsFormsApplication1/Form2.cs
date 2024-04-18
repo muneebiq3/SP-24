576f936620a84af1a504196217f0b92392118168
muneebiq3 <muneebiq526@gmail.com>
2024-04-18 17:17:43 +0500

    Visual Theory Assignment-2 and Database Lectures

---
+++ Assignments/Visual Programming/Theory/Assignment-2/Constructor/WindowsFormsApplication1/WindowsFormsApplication1/Form2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form2 : Form
+    {
+        private string passedData;
+        public Form2(string data)
+        {
+            InitializeComponent();
+            passedData = data;
+            label1.
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1.instance.tb1.Text = "set by form2";
+        }
+    }
+}
